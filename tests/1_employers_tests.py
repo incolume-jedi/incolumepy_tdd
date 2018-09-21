0b10d8bfde9aef8e5babf344bd75a57b20d51e31
@britodfbr <britodfbr@gmail.com>
2018-09-21 17:24:38 -0300

    Corrigido discrepâncias nos testes de employers

--- tests/1_employers_tests.py
+++ tests/1_employers_tests.py
@@ -1,13 +1,14 @@
-import unittest
+from unittest import TestCase, main
 import datetime as dt
 from datetime import datetime
-from employers import Employee
+from incolumepy.employers import Employee
 
 
-class Employers_tests(unittest.TestCase):
+class EmployersTests(TestCase):
 
     @classmethod
     def setUpClass(cls):
+        ...
 
     @classmethod
     def tearDownClass(cls):
@@ -21,12 +22,11 @@ class Employers_tests(unittest.TestCase):
     def tearDown(self):
         pass
 
-
     def test_domain(self):
         self.emp1.domain = 'incolume.com.br'
-        self.assertEqual(self.emp1.domain, 'incolume.com.br')
-        self.assertEqual(self.emp2.domain, 'incolume.com.br')
-        self.assertEqual(self.emp3.domain, 'incolume.com.br')
+        self.assertEqual('incolume.com.br', self.emp1.domain)
+        self.assertEqual('incolume.com.br', self.emp2.domain)
+        self.assertEqual('incolume.com.br', self.emp3.domain)
 
     def test_fristname(self):
         self.emp1.firstname = 'francisco'
@@ -36,14 +36,18 @@ class Employers_tests(unittest.TestCase):
         self.assertEqual(self.emp3.firstname, 'Marcos')
 
     def test_email(self):
-        self.assertRaises(ValueError, self.emp1.domain)
-        self.assertRaises(ValueError, self.emp2.domain)
-        self.assertRaises(ValueError, self.emp3.domain)
+        self.assertRaises(TypeError, self.emp1.domain)
+        self.assertRaises(TypeError, self.emp2.domain)
+        self.assertRaises(TypeError, self.emp3.domain)
 
         self.emp1.domain = 'incolume.com.br'
-        self.assertEqual(self.emp1.email, 'jose.silva@incolume.com.br')
-        self.assertEqual(self.emp2.email, 'maria.souza@incolume.com.br')
-        self.assertEqual(self.emp3.email, 'marcos.santos@incolume.com.br')
+        self.assertEqual('jose.silva@incolume.com.br', self.emp1.email)
+        self.assertEqual('maria.souza@incolume.com.br', self.emp2.email)
+        self.assertEqual('marcos.santos@incolume.com.br', self.emp3.email)
+
+        self.emp3.domain = 'exemplo.com.br'
+        self.assertEqual('jose.silva@exemplo.com.br', self.emp1.email)
+        self.assertEqual('maria.souza@exemplo.com.br', self.emp2.email)
 
     def test_lastname(self):
         self.assertEqual(self.emp1.lastname, 'Silva')
@@ -54,6 +58,7 @@ class Employers_tests(unittest.TestCase):
         self.assertEqual(self.emp3.lastname, 'Nascimento')
 
     def test_fullname(self):
+        self.emp1 = Employee('josé ferreira da silva', '10/10/2010', 960)
         self.assertEqual(self.emp1.fullname, 'José Ferreira da Silva')
         self.assertEqual(self.emp2.fullname, 'Maria Francisca de Souza')
         self.assertEqual(self.emp3.fullname, 'Marcos Oliveira Santos')
@@ -64,8 +69,7 @@ class Employers_tests(unittest.TestCase):
         self.assertEqual(self.emp2.fullname, 'Joana Francisca de Souza')
 
         self.emp3.lastname = 'nascimento'
-        self.assertEqual(self.emp2.fullname, 'Marco Oliveira Nascimento')
-
+        self.assertEqual(self.emp3.fullname, 'Marcos Oliveira Nascimento')
 
     def test_middlename(self):
         self.assertEqual(self.emp1.middlename, 'Ferreira da')
@@ -93,34 +97,41 @@ class Employers_tests(unittest.TestCase):
         self.assertEqual(self.emp2.salario, 1000)
         self.assertEqual(self.emp3.salario, 1000)
 
-    def test_aumento_salario(self):
+    def test_aumento_salario_int(self):
         self.emp1.aumento_salario(5)
-        self.emp1.aumento_salario(8)
-        self.emp1.aumento_salario(10)
+        self.emp2.aumento_salario(8)
+        self.emp3.aumento_salario(10)
 
-        self.assertEqual(self.emp1.salario, 960*1.05)
-        self.assertEqual(self.emp2.salario, 960*1.08)
-        self.assertEqual(self.emp3.salario, 960*1.10)
+        self.assertEqual(960*1.05, self.emp1.salario)
+        self.assertEqual(1000*1.08, self.emp2.salario)
+        self.assertEqual(1000*1.10, self.emp3.salario)
 
+    def test_aumento_salario_float(self):
+        self.emp3.aumento_salario(.1)
+        self.assertEqual(1000*1.10, self.emp3.salario)
 
     def test_date_born(self):
-        assert isinstance(self.emp1.born, datetime), "Objeto não é o instancia de datetime.datetime"
-        self.assertEqual(self.emp1.born, '06 de setembro de 2000')
-        self.assertEqual(self.emp2.born, '06 de setembro de 1978')
-        self.assertEqual(self.emp3.born, '01 de janeiro de 1985')
-
-        with self.assertRaises(ValueError):
-            self.emp3.born = datetime.today()
-            self.emp3.born = datetime.today() + dt.timedelta(days=1)
-            self.emp3.born = datetime.today() + dt.timedelta(weeks=1)
-            self.emp3.born = datetime.today() + dt.timedelta(weeks=5)
-            self.emp3.born = datetime.today() + dt.timedelta(weeks=15)
-            self.emp3.born = datetime.today() + dt.timedelta(weeks=53)
-
+        # assert isinstance(self.emp1.born, datetime), "Objeto não é instancia de datetime.datetime"
+        self.assertTrue(datetime, type(self.emp1.born))
+        self.assertEqual('06 de setembro de 2000', self.emp1.born)
+        self.assertEqual('06 de setembro de 1978', self.emp2.born)
+        self.assertEqual('01 de janeiro de 1985', self.emp3.born)
 
         self.emp2.born = '5/5/2005'
         self.assertEqual(self.emp2.born, '05 de maio de 2005')
 
+    def test_date_born_Raiser(self):
+        with self.assertRaises(ValueError):
+            self.emp3.born = datetime.today().strftime('%d/%m/%Y')
+            self.emp3.born = (datetime.today() + dt.timedelta(days=1)).strftime('%d/%m/%Y')
+            self.emp3.born = (datetime.today() + dt.timedelta(weeks=1)).strftime('%d/%m/%Y')
+            self.emp3.born = (datetime.today() + dt.timedelta(weeks=5)).strftime('%d/%m/%Y')
+            self.emp3.born = (datetime.today() + dt.timedelta(weeks=15)).strftime('%d/%m/%Y')
+            self.emp3.born = (datetime.today() + dt.timedelta(weeks=53)).strftime('%d/%m/%Y')
+
+        with self.assertRaisesRegex(ValueError, "Data de Nascimento inferior a permitida"):
+            self.emp3.born = (datetime.today() + dt.timedelta(days=1)).strftime('%d/%m/%Y')
+
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
