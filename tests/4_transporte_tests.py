73f3c5771f62637fc346fc1c2f3c71e0e9d9a2cf
@britodfbr <britodfbr@gmail.com>
2018-09-21 23:30:47 -0300

    Outra maneira de implementar classes abstratas com o pacote abc (Abstract Base Classes)

---
+++ tests/4_transporte_tests.py
@@ -0,0 +1,30 @@
+from unittest import TestCase, main
+from abc import ABC, ABCMeta
+import datetime
+from incolumepy.transporte import Transporte
+
+
+class TransporteTest(TestCase):
+
+    def setUp(self):
+        self.cls = Transporte
+
+    def tearDown(self):
+        del self.cls
+
+    def test_isInterface(self):
+        self.assertTrue(issubclass(self.cls, ABC))
+        self.assertTrue(self.cls.__metaclass__ == ABCMeta)
+
+
+    def test_instancia(self):
+        with self.assertRaisesRegex(TypeError,
+                "Can't instantiate abstract class {} with abstract methods".format(self.cls.__name__)):
+            a = self.cls()
+
+    def test_tipo_transporte(self):
+        self.assertIn('carga'.upper(), self.cls._tipo_transporte)
+        self.assertIn('passageiro'.upper(), self.cls._tipo_transporte)
+
+if __name__ == '__main__':
+    main
