3a4942a37cd4ad94deef7c83109a40866c0ce8b3
@britodfbr <britodfbr@gmail.com>
2019-12-31 09:44:12 -0300

    Ajuste na string doctest + configuração bumpversion + setup do pacote

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='incolumepy_tdd',
+      version='1.1.0-dev0',
+      description='Python Utilities for trainning',
+      author='Ricardo Brito do Nascimento',
+      author_email='contato@incolume.com.br',
+      url='https://gitlab.com/development-incolume/treinamentos/incolumepy_tdd',
+      packages=['nose'],
+     )
