8f4e0dad23f7be47bf75b6d60626293c1a825e30
@britodfbr <britodfbr@gmail.com>
2019-12-31 20:44:24 -0300

    Update version: 1.1.16-dev0 → 2.0.0-dev0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='incolumepy_tdd',
-      version='1.1.16-dev0',
+      version='2.0.0-dev0',
       description='Python Utilities for trainning',
       author='Ricardo Brito do Nascimento',
       author_email='contato@incolume.com.br',
