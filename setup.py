91e4148313b384b79fc07f7b7597642fc63bb800
@britodfbr <britodfbr@gmail.com>
2020-01-01 09:08:55 -0300

    suit de testes

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from setuptools import setup
 
 setup(name='incolumepy_tdd',
-      version='2.0.1-dev0',
+      version='2.0.1-dev1',
       description='Python Utilities for trainning',
       author='Ricardo Brito do Nascimento',
       author_email='contato@incolume.com.br',
       url='https://gitlab.com/development-incolume/treinamentos/incolumepy_tdd',
-      packages=['nose'],
+      test_suite='nose.collector',
+      tests_require='nose',
       )
