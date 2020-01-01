fbfda05a4ed5d334fdcfdc58e1e9020ff622d592
@britodfbr <britodfbr@gmail.com>
2020-01-01 09:20:06 -0300

    Unificado código base para solução números romanos

--- tests/13_romanos_tests.py
+++ tests/13_romanos_tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import unittest
-from incolumepy.utils import Romanos
+from incolumepy.romanos import Romanos
 
 
 class TestRomanos(unittest.TestCase):
