984e58e8a18ec9ecceef35fa49f337dd633b5330
@britodfbr <britodfbr@gmail.com>
2020-01-02 01:43:35 -0300

    Limpo fatorial, qui jan  2 01:43:35 -03 2020

--- incolumepy/sequences/fatorial.py
+++ incolumepy/sequences/fatorial.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import math
-
-
-def fatorial(n):
-    if n < 0:
-        raise ValueError("n must be >= 0")
-    if math.floor(n) != n:
-        raise ValueError("n must be exact integer")
-    if n + 1 == n:  # catch a value like 1e300
-        raise OverflowError("n too large")
-    if n <= 1:
-        return 1
-    return n * fatorial(n - 1)
 
 
 if __name__ == "__main__":
