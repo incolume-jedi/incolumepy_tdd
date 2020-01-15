b515e137b950b92964dc08e713ae2dda70e53749
@britodfbr <py@incolume.com.br>
2020-01-15 08:15:13 -0300

    Unit Test para hashes(implementação inicial), qua jan 15 08:15:13 -03 2020

--- tests/22_hashes_tests.py
+++ tests/22_hashes_tests.py
@@ -1,9 +1,14 @@
 import unittest
-
+import os
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    @classmethod
+    def setUpClass(cls) -> None:
+        base = os.path.dirname(__file__)
+        cls.file1 = os.path.join(base, 'static_html', 'css', 'legis_3.css')
+
+    def test_hash_0(self):
+        self.assertEqual('abc', hash_md5_0(self.file1))
 
 
 if __name__ == '__main__':
