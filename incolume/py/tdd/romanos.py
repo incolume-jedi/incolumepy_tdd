2f915ce964babb13d2a5395ce313f651cc02b8b9
Britodfbr <britodfbr@gmail.com>
2023-11-22 08:39:42 -0300

    chore!: Nova definição de namespace para projeto
    BREAKING CHANGE: possível quebra de continuidade na execução de testes

---
+++ incolume/py/tdd/romanos.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Exercício com doctest
+Habilidade com classes python
+
+dica: solução ideal 3 linhas
+"""
+
+# TODO: Atividade  13: implementar Romanos para que passe nos testes
+
+class Romanos:
+    """Class Romanos."""
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testfile("13-romanos.txt")
