d4762b422aa03aea73f2911164ebda01b20e38c4
Britodfbr <britodfbr@gmail.com>
2023-11-22 22:03:31 -0300

    feat: Recuperado TDD 34_menu_terminal_tests

---
+++ tests/34_menu_terminal_tests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# TODO: Atividade 30: Proceder com as implementações necessárias para que passe nos testes
+
+"""
+__author__ = '@britodfbr'
+
+import unittest
+from incolumepy.tdd.menu import entrance, itemmenu, line, header
+
+
+@unittest.skip
+class MyTestCase(unittest.TestCase):
+    def test_entrance(self):
+        # TODO: pendente
+        ...
+
+    def test_itemmenu(self):
+        # TODO: pendente
+        ...
+
+    def test_line(self):
+        # TODO: pendente
+        ...
+
+    def test_header(self):
+        # TODO: pendente
+        ...
+
+
+if __name__ == '__main__':
+    unittest.main()
